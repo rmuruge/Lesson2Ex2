55cda2b1d687dc15c989433ffbfdcb66ecacd1f4
rmuruge <53324361+rmuruge@users.noreply.github.com>
2019-07-26 11:49:32 -0500

    Update ProgramJavaOne.java
    Added a SOP for github change

--- ProgramJavaOne.java
+++ ProgramJavaOne.java
@@ -1,6 +1,7 @@
 class ProgramJavaOne {
  public static void mail (String[] args) {
- System.out.println ("Sample java program for Git Tutorial");
+  System.out.println ("Sample java program for Git Tutorial");
+  System.out.println ("Added this comment in GitHub");
 }
 
 }
