705eeee1a58bf6056ae1da4f363e229329711fd1
rmuruge <53324361+rmuruge@users.noreply.github.com>
2019-07-28 11:37:18 -0500

    Origin 2 Exercise 2 commit

--- ProgramJavaOne.java
+++ ProgramJavaOne.java
@@ -2,6 +2,8 @@ class ProgramJavaOne {
  public static void mail (String[] args) {
   System.out.println ("Sample java program for Git Tutorial");
   System.out.println ("Added this comment in GitHub");
+  System.out.println ("Origin 2 Exercise 2 commit");
+
 }
 
 }
