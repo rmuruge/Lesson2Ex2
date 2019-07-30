7f790375c1ad4307701f3162d14d9151773c61d2
Ubuntu <rmuruge@gmail.com>
2019-07-30 21:05:43 +0000

    changed java

--- ProgramJavaOne.java
+++ ProgramJavaOne.java
@@ -1,4 +1,4 @@
-class ProgramJavaOne {
+lass ProgramJavaOne {
  public static void mail (String[] args) {
   System.out.println ("Sample java program for Git Tutorial");
   System.out.println ("Added this comment in GitHub");
